7d5435032758ab6dee6f05e41d77dcf5152579fe
= <=>
2024-04-24 21:01:46 +0200

    Añadiendo bootstrap y estilos

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./footer";
 import GestionBicicletas from "./GestionBicicletas";
-import Register from "./register";
+import RegistrarUsuario from "./RegistrarUsuario";
 import Header from "./header";
 import './App.css';
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
     <Header />
     <main>
-    <Register></Register>
+    <RegistrarUsuario/>
     </main>
     <Footer />
   </div>
--- cliente/src/RegistrarUsuario.js
+++ cliente/src/RegistrarUsuario.js
@@ -1,8 +1,8 @@
-
+import './registro.css'
 import React from 'react';
 import {useForm } from 'react-hook-form';
 
-function Register() {
+function RegistrarUsuario() {
     const { register, handleSubmit } = useForm();
 
     function onSubmit(data) {
@@ -24,44 +24,44 @@ function Register() {
     }
 
     return (
-        <div class="container mt-5">
+        <div class="container-fluid form-margin">
             <div class="row justify-content-center">
-                <div class="col-md-8">
+                <div class="col-md-5">
                     <div class="card">
                         <div class="card-header">
                             <h2 class="text-center">Registro</h2>
                         </div>
                         <div class="card-body">
                             <form onSubmit={handleSubmit(onSubmit)}>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <label for="nombre" class="col-sm-3 col-form-label">Nombre:</label>
                                     <div class="col-sm-9">
                                         <input type="text" class="form-control" id="nombre" name="nombre" required 
                                         {...register('nombre')}/>
                                     </div>
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <label for="apellidos" class="col-sm-3 col-form-label">Apellidos:</label>
                                     <div class="col-sm-9">
                                         <input type="text" class="form-control" id="apellidos" name="apellidos" required
                                         {...register('apellidos')}/>
                                     </div>
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <label for="correo" class="col-sm-3 col-form-label">Correo electrónico:</label>
                                     <div class="col-sm-9">
                                         <input type="email" class="form-control" id="correo" name="correo" required
                                         {...register('correo')}/>
                                     </div>
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <label for="clave" class="col-sm-3 col-form-label">Clave:</label>
                                     <div class="col-sm-9">
                                         <input type="password" class="form-control" id="clave" name="clave" required
                                         {...register('clave')}/>
                                     </div>
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <label for="fechaNacimiento" class="col-sm-3 col-form-label">Fecha de nacimiento:</label>
                                     <div class="col-sm-9">
                                         <input type="date" class="form-control" id="fechaNacimiento"
@@ -69,7 +69,7 @@ function Register() {
                                             {...register('fechaNacimiento')}/>
                                     </div>
                                 </div>
-                                <div class="form-group row">
+                                <div class="form-group row mb-4">
                                     <div class="col-sm-12 text-center">
                                         <button type="submit" class="btn btn-primary">Registrarse</button>
                                     </div>
@@ -83,4 +83,4 @@ function Register() {
     );
 }
 
-export default Register;
+export default RegistrarUsuario;
