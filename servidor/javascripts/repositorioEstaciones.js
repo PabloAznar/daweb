7817bfa6b1d977c26361c642656039aa07173abf
= <=>
2024-05-02 19:54:05 +0200

    Adding eliminar estaciones con plantilla hbs

--- servidor/javascripts/repositorioEstaciones.js
+++ servidor/javascripts/repositorioEstaciones.js
@@ -13,14 +13,32 @@ async function getConnection() {
     return connection
 }
 
-    async function registrarEstacion(connection, nombre, capacidad, codigo_postal, fechaAlta) {
+    async function registrarEstacion(connection, nombre, capacidad, codigo_postal) {
         let result = connection.execute("INSERT INTO ESTACION_APARCAMIENTO SET nombre = ?, capacidad = ?, numero_bicicletas = ?, codigo_postal = ?, fecha_alta = ?",
   [nombre, capacidad, BICICLETAS_POR_DEFECTO, codigo_postal, new Date()]);
 
         return result
     }
 
+    async function modificarEstacion(connection, id, nombre, capacidad, numeroBicicletas, codigo_postal, fechaAlta) {
+        let result = connection.execute("INSERT INTO ESTACION_APARCAMIENTO SET id = ?, nombre = ?, capacidad = ?, numero_bicicletas = ?, codigo_postal = ?, fecha_alta = ?",
+  [id, nombre, capacidad, numeroBicicletas, codigo_postal, fechaAlta]);
+        return result;
+    }
 
+    async function eliminarEstacion(connection, id) {
+        let result = connection.execute("DELETE FROM ESTACION_APARCAMIENTO WHERE id = ?",
+    [id]);
+    return result;
+    }
+
+    async function obtenerEstaciones(connection) {
+        const resultados = await connection.execute("SELECT * FROM ESTACION_APARCAMIENTO")
+        return resultados[0]
+    }
 
 exports.getConnection = getConnection
 exports.registrarEstacion = registrarEstacion
+exports.modificarEstacion = modificarEstacion
+exports.eliminarEstacion = eliminarEstacion
+exports.obtenerEstaciones = obtenerEstaciones
--- servidor/routes/estaciones.js
+++ servidor/routes/estaciones.js
@@ -22,4 +22,48 @@ var app = express()
     .catch(error => {console.log(error)});
   });
 
+  router.put('/', function(req, res, next) {
+    let id = req.body.id
+    let nombre = req.body.nombre
+    let capacidad = req.body.capacidad
+    let numeroBicicletas = req.body.numeroBicicletas
+    let codigoPostal = req.body.codigoPostal
+    let fechaAlta = req.body.fechaAlta
+
+    bbdd.getConnection()
+    .then(con => {
+        return bbdd.modificarEstacion(con, id, nombre, capacidad, numeroBicicletas, codigoPostal, fechaAlta)
+    })
+    .then(result => {
+        res.send("Estacion modificada")
+    })
+    .catch(error => {console.log(error)})
+  });
+
+  router.delete("/:id", function(req, res, next) {
+    let id = req.params.id;
+
+    bbdd.getConnection()
+    .then(con => {
+        return bbdd.eliminarEstacion(con, id)
+    })
+    .then(result => {
+        res.send("Estacion eliminada")
+    })
+    .catch(error => {console.log(error)})
+
+  })
+
+  router.get("/", function(req, res, next) {
+    bbdd.getConnection()
+    .then(con => {
+        return bbdd.obtenerEstaciones(con)
+    })
+    .then(data => {
+        console.log(data)
+        res.render('estaciones', {'tittle' : 'estaciones', 'estacion' : data} )
+    })
+    .catch(error => {console.log(error)})
+  })
+
   module.exports = router;
